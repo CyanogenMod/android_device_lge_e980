d3406a5b99a7c6f4cea17d7a3a5e9e541b95e520
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-09-28 22:52:57 +0100

    cm: Fix stupid typos

--- cm.mk
+++ cm.mk
@@ -18,10 +18,10 @@ $(call inherit-product, device/lge/e980/e980.mk)
 PRODUCT_DEVICE := e980
 PRODUCT_NAME := cm_e980
 PRODUCT_BRAND := lge
-PRODUCT_MODEL := LG-E986
+PRODUCT_MODEL := LG-E980
 PRODUCT_MANUFACTURER := LGE
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=>geefhd_open_eu BUILD_FINGERPRINT=lge/geefhd_open_eu/geefhd:4.1.2/JZO54K/E98610b.1373081564:user/release-keys PRIVATE_BUILD_DESC="geefhd_open_eu-user 4.1.2 JZO54K E98610b.1373081564 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=geefhd_open_eu BUILD_FINGERPRINT=lge/geefhd_open_eu/geefhd:4.1.2/JZO54K/E98610b.1373081564:user/release-keys PRIVATE_BUILD_DESC="geefhd_open_eu-user 4.1.2 JZO54K E98610b.1373081564 release-keys"
 
 # Enable Torch
 PRODUCT_PACKAGES += Torch
