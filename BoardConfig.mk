66958dced7356b5413eaffc8eef330971f9a1cdc
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-12 02:44:19 +0000

    Updates for 11.0
    * Disable QCOM_BSP
    * update sepolicies
    * update wifi service declarations
    * update sdcard service declarations
    * General vold updates
    
    Change-Id: Ie087458092a9c910da76e28b7bbf6e4d84008251

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,35 +51,25 @@ COMMON_GLOBAL_CFLAGS += -DLG_CAMERA_HARDWARE
 BOARD_SEPOLICY_DIRS += \
         device/lge/e980/sepolicy
 
-BOARD_SEPOLICY_UNION += \
-	file_contexts \
-	property_contexts \
-	te_macros \
-	bluetooth_loader.te \
-	bridge.te \
-	camera.te \
-	conn_init.te \
-	device.te \
-	dhcp.te \
-	domain.te \
-	drmserver.te \
-	file.te \
-	kickstart.te \
-	init.te \
-	mediaserver.te \
-	mpdecision.te \
-	netmgrd.te \
-	property.te \
-	qmux.te \
-	rild.te \
-	rmt.te \
-	sensors.te \
-	surfaceflinger.te \
-	system.te \
-	tee.te \
-	thermald.te \
-	ueventd.te \
-	wpa_supplicant.te
+BOARD_SEPOLICY_UNION := \
+        app.te \
+        bluetooth.te \
+        device.te \
+        domain.te \
+        drmserver.te \
+        file.te \
+        file_contexts \
+        hci_init.te \
+        init_shell.te \
+        keystore.te \
+        mediaserver.te \
+        kickstart.te \
+        nfc.te \
+        rild.te \
+        surfaceflinger.te \
+        system.te \
+        ueventd.te \
+        wpa.te
 
 TARGET_OTA_ASSERT_DEVICE := e986,e980,geefhd,e988,gkatt
 
