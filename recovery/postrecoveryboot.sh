ee19519b82bd34dbd4de8ee9b713135893d4b96c
Seth Shelnutt <shelnutt2@gmail.com>
2014-02-08 17:02:00 -0500

    Remove misc flag when booting into recovery
    Change-Id: Ib155d947d28dc4bacddb30145b3f51d27cb6f271

--- recovery/postrecoveryboot.sh
+++ recovery/postrecoveryboot.sh
@@ -4,3 +4,15 @@
 echo 120 > /sys/class/leds/lcd-backlight/brightness
 echo 5 > /sys/class/leds/button-backlight/brightness
 echo "0x404010 -1 -1" > /sys/class/leds/R/device/led_blink
+
+# wait for system to settle
+sleep 5
+
+# Tell us that we're in postrecoveryboot.sh
+echo "I:Postrecoveryboot script started!" >> /cache/recovery/log
+
+# Clear "recovery --bnr_recovery-s" or etc flag.
+busybox dd if=/dev/zero of=/dev/block/platform/msm_sdcc.1/by-name/misc conv=notrunc bs=1 count=32 seek=64
+
+# Tell us that it worked!
+echo "I:LGE MISC flag reset" >> /cache/recovery/log
